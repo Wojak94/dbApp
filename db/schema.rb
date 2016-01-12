d0b43b7f39e9ce2a531732d5367dad03bd53794f
Wojak94 <wojciechadles@gmail.com>
2016-01-12 20:11:36 +0100

    dodany password_conf dla User

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112164323) do
+ActiveRecord::Schema.define(version: 20160112180000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,8 +98,9 @@ ActiveRecord::Schema.define(version: 20160112164323) do
     t.string   "password"
     t.float    "money_status"
     t.integer  "location_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.string   "password_confirmation"
   end
 
 end
